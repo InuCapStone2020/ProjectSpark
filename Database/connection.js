b8ef7e98bbcd734a0450ba25e8fda20c22407e74
ComelyU <31150286+ComelyU@users.noreply.github.com>
2021-02-08 06:26:38 +0900

    update

---
+++ Database/connection.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+//interlock to mysql
+var connection = mysql.createConnection({
+    host: "[endpoint address]",
+    user: "[masteruser id]",
+    database: "[databse to access]",
+    password: "[pw]",
+    port: 3306
+});
