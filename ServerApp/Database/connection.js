8e8a4b0d6a02e573150953487597fcf424e8cb1c
ComelyU <31150286+ComelyU@users.noreply.github.com>
2021-02-08 05:39:35 +0900

    update

--- ServerApp/Database/connection.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-//interlock to mysql
-var connection = mysql.createConnection({
-    host: "[endpoint address]",
-    user: "[masteruser id]",
-    database: "[databse to access]",
-    password: "[pw]",
-    port: 3306
-});
--- ServerApp/Database/json_parse.js
+++
@@ -1,9 +0,0 @@
-var json = '{"NUM":11111, "SUBNUM":2, "M_DATE":"2020-03-09", "M_TIME":"03:09:39", "REGION":"인천광역시 연수구", "CONTENT":"COVID-19"}';
-var obj = JSON.parse(json);
-
-console.log(obj.NUM);
-console.log(obj.SUBNUM);
-console.log(obj.M_DATE);
-console.log(obj.M_TIME);
-console.log(obj.REGION);
-console.log(obj.CONTENT);
