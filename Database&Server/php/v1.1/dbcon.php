f83e813f619077908f512ea3c86f598d5ccc0ccb
ComelyU <31150286+ComelyU@users.noreply.github.com>
2021-04-09 20:21:18 +0900

    update

---
+++ Database&Server/php/v1.1/dbcon.php
@@ -0,0 +1,26 @@
+<?php
+ 
+    $host = 'sparkdb.cm69yl9n1hcy.us-east-1.rds.amazonaws.com'; #end point
+    $username = 'spark'; # MySQL 계정 아이디
+    $password = '#spark16'; # MySQL 계정 패스워드
+    $dbname = 'spark';  # DATABASE 이름
+ 
+ 
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    
+    try {
+ 
+        $con = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8",$username, $password);
+    } catch(PDOException $e) {
+ 
+        die("Failed to connect to the database: " . $e->getMessage()); 
+    }
+ 
+ 
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+ 
+ 
+    header('Content-Type: text/html; charset=utf-8'); 
+    #session_start();
+?>
