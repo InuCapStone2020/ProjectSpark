b98613018c2e019d6dc3cfa178ee3b95a7a87c9e
ComelyU <31150286+ComelyU@users.noreply.github.com>
2021-04-18 20:34:55 +0900

    node.js update

---
+++ Database&Server/js/spark.js
@@ -0,0 +1,280 @@
+var mysql = require("mysql");
+var express = require("express");
+var bodyParser = require("body-parser"); //using body-parser for body parsing
+var app = express();
+
+app.use(bodyParser.json()); //format is json
+app.use(bodyParser.urlencoded({ extended: true })); //querystring module
+
+//using Port 3000 for listening
+app.listen(3000, () => {
+    console.log("Server is acting...");
+});
+
+//interlock to mysql
+var connection = mysql.createConnection({
+    host: "sparkdb.cm69yl9n1hcy.us-east-1.rds.amazonaws.com", //end point address
+    user: "spark", //masteruser id
+    database: "spark", //database to access
+    password: "#spark16", //database pw
+    port: 3306
+});
+
+app.post("/messageinsert", (req, res) => {
+    var number = req.body.data.length;
+
+    var num = [];
+    var subNum = [];
+    var date = [];
+    var time = [];
+    var region = [];
+    var content = [];
+    var event = [];
+
+    var qs = [];
+
+    var i = 0;
+    while (i < number) {
+        num.push(req.body.data[i].NUM);
+        subNum.push(req.body.data[i].SUB_NUM);
+        date.push(req.body.data[i].M_DATE);
+        time.push(req.body.data[i].M_TIME);
+        region.push(req.body.data[i].REGION);
+        content.push(req.body.data[i].CONTENT);
+        event.push(req.body.data[i].EVENT);
+        var temp = [num[i], subNum[i], date[i], time[i], region[i], content[i], event[i]];
+        qs.push(temp);
+        i++;
+    }
+
+    //sql for insert to db
+    var insertSQL = "INSERT INTO Message_List (NUM, SUBNUM, M_DATE, M_TIME, REGION, CONTENT, EVENT) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    var j = 0;
+    var ret = "Error";
+    while (j < number) {
+        connection.query(insertSQL, qs[j], (err, result) => {
+            if (err) {
+                ret = "Error";
+                console.log(err);
+            } else {
+                ret = "Insert Successful";
+            }
+        });
+        j++;
+    }
+    res.json({
+        message: ret
+    });
+});
+
+app.get("/search", (req, res) => {
+    var region = req.body.region;
+    if (region == "") {
+        region = "전체 전체";
+    }
+    var s_date = req.body.sdate;
+    if (s_date == "") {
+        s_date = "2000-01-01";
+    }
+    var e_date = req.body.edate;
+    if (e_date == "") {
+        e_date = "9999-12-31";
+    }
+    var event = req.body.event;
+    if (event == "") {
+        event = ["전염병", "자연 재해", "기타"];
+    }
+    var page = req.body.page;
+    if (page == "") {
+        page = 1;
+    }
+    var offset = (page - 1) * 10;
+
+    var cnt = [];
+
+    if (region != "전체 전체") {
+        var splitRegion = region.split("','");
+        var cnt = splitRegion.length;
+        var i = 0;
+        while (i < cnt) {
+            var spt = splitRegion[i].split(" ");
+            if (spt[1] != "전체") {
+                splitRegion.push(spt[0] + " 전체");
+                i = i + 1;
+            } else {
+                splitRegion.splice(i, 1, "^" + spt[0]);
+            }
+        }
+        var mergeRegion = splitRegion.join("|");
+        region = mergeRegion;
+
+        if (page == 1) {
+            var countSQL = "select count(*) as count from Message_List where (region REGEXP ?) and (m_date between ? and ?) and (event in(?))";
+            var qs = [region, s_date, e_date, event];
+
+            connection.query(countSQL, qs, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    cnt = result;
+                }
+            });
+        }
+
+        var searchSQL = "select * from Message_List where (region REGEXP ?) and (m_date between ? and ?) and (event in(?)) order by num desc, subnum asc limit ?, 10";
+        qs = [region, s_date, e_date, event, offset];
+
+        connection.query(searchSQL, qs, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({
+                    cnt,
+                    result
+                });
+            }
+        });
+    } else {
+        if (page == 1) {
+            var countSQL = "select count(*) as count from Message_List where (m_date between ? and ?) and (event in(?))";
+            var qs = [s_date, e_date, event];
+
+            connection.query(countSQL, qs, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    cnt = result;
+                }
+            });
+        }
+        var searchSQL = "select * from Message_List where (m_date between ? and ?) and (event in(?)) order by num desc, subnum asc limit ?, 10";
+        qs = [s_date, e_date, event, offset];
+
+        connection.query(searchSQL, qs, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({
+                    cnt,
+                    result
+                });
+            }
+        });
+    }
+});
+
+app.get("/mindate", (req, res) => {
+    var mindateSQL = "SELECT MIN(M_DATE) as mindate from Message_List";
+
+    connection.query(mindateSQL, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.get("/weekcount", (req, res) => {
+    var weekcountSQL =
+        "select m1.region as region, count(*) as count from (select region from Message_List where m_date between subdate(curdate(),6) and curdate() group by region) as m1, (select region from Message_List where m_date between subdate(curdate(),6) and curdate()) as m2 where case when substring_index(m1.region, ' ', -1) != '전체' then m2.region in (concat(substring_index(m1.region, ' ', 1), ' 전체'), m1.region) when substring_index(m1.region, ' ', -1)='전체' then m2.region like concat(substring_index(m1.region, ' ', 1),'%') END group by m1.region order by m1.region";
+
+    connection.query(weekcountSQL, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.get("/weekdetail", (req, res) => {
+    var region = req.body.region;
+
+    var splitRegion = region.split("','");
+    var cnt = splitRegion.length;
+    var i = 0;
+    while (i < cnt) {
+        var spt = splitRegion[i].split(" ");
+        if (spt[1] != "전체") {
+            splitRegion.push(spt[0] + " 전체");
+            i = i + 1;
+        } else {
+            splitRegion.splice(i, 1, "^" + spt[0]);
+        }
+    }
+    var mergeRegion = splitRegion.join("|");
+    region = mergeRegion;
+
+    var weekdetailSQL = "select * from Message_List where (m_date between subdate(curdate(),6) and curdate()) and (region REGEXP ?) order by num desc, subnum asc";
+    var qs = [region];
+
+    connection.query(weekdetailSQL, qs, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.get("/notice", (req, res) => {
+    var noticeSQL = "";
+
+    var region = req.body.region;
+    if (region == "") {
+        region = "전체 전체";
+    }
+
+    if (region != "전체 전체") {
+        var splitRegion = region.split("','");
+        var cnt = splitRegion.length;
+        var i = 0;
+        while (i < cnt) {
+            var spt = splitRegion[i].split(" ");
+            if (spt[1] != "전체") {
+                splitRegion.push(spt[0] + " 전체");
+                i = i + 1;
+            } else {
+                splitRegion.splice(i, 1, "^" + spt[0]);
+            }
+        }
+        var mergeRegion = splitRegion.join("|");
+        region = mergeRegion;
+
+        noticeSQL =
+            "select num, subnum, m_date, m_time, region, content, event from (select *, concat(m_date,' ', m_time) as m_dt from Message_List) as ml_dt where (ml_dt.m_dt > DATE_SUB(now(),INTERVAL 1 hour)) and (region REGEXP ?)";
+    } else {
+        noticeSQL =
+            "select num, subnum, m_date, m_time, region, content, event from (select *, concat(m_date,' ', m_time) as m_dt from Message_List) as ml_dt where ml_dt.m_dt > DATE_SUB(now(),INTERVAL 1 hour)";
+    }
+
+    var qs = [region];
+
+    connection.query(noticeSQL, qs, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.patch("/eventupdate", (req, res) => {
+    var event = req.body.event;
+    var num = req.body.num;
+    var subnum = req.body.subnum;
+
+    var updateSQL = "UPDATE Message_List SET EVENT=? WHERE NUM=? AND SUBNUM=?";
+    var qs = [event, num, subnum];
+
+    ret = "Update Success";
+    connection.query(updateSQL, qs, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(ret);
+        }
+    });
+});
